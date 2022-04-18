f691e5d09f1d89b6fed96d656ec6e140a843e3d9
onlinebacon <bacon.antife@gmail.com>
2022-04-17 23:28:53 -0300

    Trilaterate sphere

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-import { Vec3, Mat3 } from './l-algebra-3.js';
+import trilaterate from './trilaterate-sphere.js';
 
-import {
-	chordToArclen,
-	coordToNormalVec3,
-	normalVec3ToCoord,
-	calcDist,
-} from './sphere-math.js';
+let args = [
+	{
+		"gp": [
+			-1.0631469516777392,
+			-1.8405047363718645
+		],
+		"arc": 1.1663949573238783
+	},
+	{
+		"gp": [
+			0.48820980094570826,
+			2.5235437683133997
+		],
+		"arc": 1.0840464293852248
+	}
+];
 
-const coord = (lat, lon) => [ lat/180*Math.PI, lon/180*Math.PI ];
+args = args.map(arg => {
+	const { gp, arc } = arg;
+	const [ lat, lon ] = gp;
+	return { lat, lon, distance: arc };
+});
+
+const res = trilaterate({ points: args })
+	.map(coord => coord.map(val => val*180/Math.PI));
+
+res.sort((a, b) => a[0] - b[0]).splice(1, 1);
+
+console.log(res.map(coord => coord.map(x => x.toFixed(5)).join(', ')).join('\n'));
