03fec619e082af6cea08ae75b7051f12a8cf6b7b
onlinebacon <bacon.antife@gmail.com>
2022-04-25 01:55:49 -0300

    Linear Algebra 3D: Rotations optimized (done directly)

--- l-algebra-3.js
+++ l-algebra-3.js
@@ -54,6 +54,25 @@ const zRotationMat = (angle, dst) => {
 	dst[8] = 1;
 };
 
+const zxyRotationMat = (zAngle, xAngle, yAngle, dst) => {
+	const cos_z = Math.cos(zAngle);
+	const sin_z = Math.sin(zAngle);
+	const cos_x = Math.cos(xAngle);
+	const sin_x = Math.sin(xAngle);
+	const cos_y = Math.cos(yAngle);
+	const sin_y = Math.sin(yAngle);
+	dst[0] = cos_z*cos_y + sin_z*sin_x*sin_y;
+	dst[1] = sin_z*cos_x;
+	dst[2] = sin_z*sin_x*cos_y - cos_z*sin_y;
+	dst[3] = cos_z*sin_x*sin_y - sin_z*cos_y;
+	dst[4] = cos_z*cos_x;
+	dst[5] = sin_z*sin_y + cos_z*sin_x*cos_y;
+	dst[6] = cos_x*sin_y;
+	dst[7] = -sin_x;
+	dst[8] = cos_x*cos_y;
+	return dst;
+};
+
 const mulVecMat = (v, m, dst) => {
 	const [ x, y, z ] = v;
 	const [ ix, iy, iz, jx, jy, jz, kx, ky, kz ] = m;
@@ -95,26 +114,67 @@ class Matrix3 extends Array {
 		zRotationMat(angle, this);
 		return this;
 	}
-	rotationZXY(zAngle, xAngle, yAngle, dst = this) {
-		return this.rotationZ(zAngle, dst).rotateX(xAngle).rotateY(yAngle);
+	rotationZXY(zAngle, xAngle, yAngle) {
+		zxyRotationMat(zAngle, xAngle, yAngle, this)
+		return this;
 	}
 	rotateX(angle, dst = this) {
-		xRotationMat(angle, auxMat);
-		this.apply(auxMat, dst);
+		const [ ix, iy, iz, jx, jy, jz, kx, ky, kz ] = this;
+		const cos = Math.cos(angle);
+		const sin = Math.sin(angle);
+		dst[0] = ix;
+		dst[1] = iy*cos - iz*sin;
+		dst[2] = iy*sin + iz*cos;
+		dst[3] = jx;
+		dst[4] = jy*cos - jz*sin;
+		dst[5] = jy*sin + jz*cos;
+		dst[6] = kx;
+		dst[7] = ky*cos - kz*sin;
+		dst[8] = ky*sin + kz*cos;
 		return dst;
 	}
 	rotateY(angle, dst = this) {
-		yRotationMat(angle, auxMat);
-		this.apply(auxMat, dst);
+		const [ ix, iy, iz, jx, jy, jz, kx, ky, kz ] = this;
+		const cos = Math.cos(angle);
+		const sin = Math.sin(angle);
+		dst[0] = ix*cos + iz*sin;
+		dst[1] = iy;
+		dst[2] = iz*cos - ix*sin;
+		dst[3] = jx*cos + jz*sin;
+		dst[4] = jy;
+		dst[5] = jz*cos - jx*sin;
+		dst[6] = kx*cos + kz*sin;
+		dst[7] = ky;
+		dst[8] = kz*cos - kx*sin;
 		return dst;
 	}
 	rotateZ(angle, dst = this) {
-		zRotationMat(angle, auxMat);
-		this.apply(auxMat, dst);
+		const [ ix, iy, iz, jx, jy, jz, kx, ky, kz ] = this;
+		const cos = Math.cos(angle);
+		const sin = Math.sin(angle);
+		dst[0] = ix*cos - iy*sin;
+		dst[1] = ix*sin + iy*cos;
+		dst[2] = iz;
+		dst[3] = jx*cos - jy*sin;
+		dst[4] = jx*sin + jy*cos;
+		dst[5] = jz;
+		dst[6] = kx*cos - ky*sin;
+		dst[7] = kx*sin + ky*cos;
+		dst[8] = kz;
 		return dst;
 	}
-	rotateZXY(zAngle, xAngle, yAngle, dst = this) {
-		return this.rotateZ(zAngle, dst).rotateX(xAngle).rotateY(yAngle);
+	clone() {
+		const clone = new Matrix3();
+		clone[0] = this[0];
+		clone[1] = this[1];
+		clone[2] = this[2];
+		clone[3] = this[3];
+		clone[4] = this[4];
+		clone[5] = this[5];
+		clone[6] = this[6];
+		clone[7] = this[7];
+		clone[8] = this[8];
+		return clone;
 	}
 }
 
@@ -142,31 +202,83 @@ class Vector3 extends Array {
 		return dst;
 	}
 	rotateX(angle, dst = this) {
-		xRotationMat(angle, auxMat);
-		this.apply(auxMat, dst);
+		const [ x, y, z ] = this;
+		const cos = Math.cos(angle);
+		const sin = Math.sin(angle);
+		dst[0] = x;
+		dst[1] = y*cos - z*sin;
+		dst[2] = y*sin + z*cos;
 		return dst;
 	}
 	rotateY(angle, dst = this) {
-		yRotationMat(angle, auxMat);
-		this.apply(auxMat, dst);
+		const [ x, y, z ] = this;
+		const sin = Math.sin(angle);
+		const cos = Math.cos(angle);
+		dst[0] = x*cos + z*sin;
+		dst[1] = y;
+		dst[2] = z*cos - x*sin;
 		return dst;
 	}
 	rotateZ(angle, dst = this) {
-		zRotationMat(angle, auxMat);
-		this.apply(auxMat, dst);
+		const [ x, y, z ] = this;
+		const sin = Math.sin(angle);
+		const cos = Math.cos(angle);
+		dst[0] = x*cos - y*sin;
+		dst[1] = x*sin + y*cos;
+		dst[2] = z;
 		return dst;
 	}
 	rotateXY(xAngle, yAngle, dst = this) {
-		return this.rotateX(xAngle, dst).rotateY(yAngle);
+		const [ x, y, z ] = this;
+		const cos_x = Math.cos(xAngle);
+		const sin_x = Math.sin(xAngle);
+		const cos_y = Math.cos(yAngle);
+		const sin_y = Math.sin(yAngle);
+		const a = y*sin_x + z*cos_x;
+		dst[0] = x*cos_y + a*sin_y;
+		dst[1] = y*cos_x - z*sin_x;
+		dst[2] = a*cos_y - x*sin_y;
+		return dst;
 	}
 	rotateXZ(xAngle, zAngle, dst = this) {
-		return this.rotateX(xAngle, dst).rotateZ(zAngle);
+		const [ x, y, z ] = this;
+		const cos_x = Math.cos(xAngle);
+		const sin_x = Math.sin(xAngle);
+		const cos_z = Math.cos(zAngle);
+		const sin_z = Math.sin(zAngle);
+		const a = y*cos_x - z*sin_x;
+		dst[0] = x*cos_z - a*sin_z;
+		dst[1] = x*sin_z + a*cos_z;
+		dst[2] = y*sin_x + z*cos_x;
+		return dst;
 	}
 	rotateYX(yAngle, xAngle, dst = this) {
-		return this.rotateY(yAngle, dst).rotateX(xAngle);
+		const [ x, y, z ] = this;
+		const cos_y = Math.cos(yAngle);
+		const sin_y = Math.sin(yAngle);
+		const cos_x = Math.cos(xAngle);
+		const sin_x = Math.sin(xAngle);
+		const a = x*-sin_y + z*cos_y;
+		dst[0] = x*cos_y + z*sin_y;
+		dst[1] = y*cos_x - a*sin_x;
+		dst[2] = y*sin_x + a*cos_x;
+		return dst;
 	}
 	rotateZXY(zAngle, xAngle, yAngle, dst = this) {
-		return this.rotateZ(zAngle, dst).rotateX(xAngle).rotateY(yAngle);
+		const [ x, y, z ] = this;
+		const cos_z = Math.cos(zAngle);
+		const sin_z = Math.sin(zAngle);
+		const cos_x = Math.cos(xAngle);
+		const sin_x = Math.sin(xAngle);
+		const cos_y = Math.cos(yAngle);
+		const sin_y = Math.sin(yAngle);
+		const a = x*sin_z + y*cos_z;
+		const b = x*cos_z - y*sin_z;
+		const c = a*sin_x + z*cos_x;
+		dst[0] = b*cos_y + c*sin_y;
+		dst[1] = a*cos_x - z*sin_x;
+		dst[2] = c*cos_y - b*sin_y;
+		return dst;
 	}
 	sub(vec, dst = this) {
 		dst[0] = this[0] - vec[0];
